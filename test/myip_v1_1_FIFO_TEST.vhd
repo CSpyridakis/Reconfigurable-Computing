e5d538a2ef91128be88f76fab2baa49ac6480bbb
Spyridakis Christos <spyridakischristos@gmail.com>
2019-03-18 18:41:56 -0800

    FIFO testbench and simulation added

--- test/myip_v1_1_FIFO_TEST.vhd
+++ test/myip_v1_1_FIFO_TEST.vhd
@@ -70,7 +70,7 @@ ARCHITECTURE behavior OF myip_v1_1_FIFO_TEST IS
 
    -- Clock period definitions
    constant FIFO_W_ACLK_period : time := 10 ns;
-   constant FIFO_R_ACLK_period : time := 10 ns;
+   constant FIFO_R_ACLK_period : time := 20 ns;
  
 BEGIN
  
@@ -90,41 +90,125 @@ BEGIN
    -- Clock process definitions
    FIFO_W_ACLK_process :process
    begin
-		FIFO_W_ACLK <= '0';
-		wait for FIFO_W_ACLK_period/2;
 		FIFO_W_ACLK <= '1';
+		wait for FIFO_W_ACLK_period/2;
+		FIFO_W_ACLK <= '0';
 		wait for FIFO_W_ACLK_period/2;
    end process;
  
    FIFO_R_ACLK_process :process
    begin
-		FIFO_R_ACLK <= '0';
-		wait for FIFO_R_ACLK_period/2;
 		FIFO_R_ACLK <= '1';
+		wait for FIFO_R_ACLK_period/2;
+		FIFO_R_ACLK <= '0';
 		wait for FIFO_R_ACLK_period/2;
    end process;
  
 
    -- Stimulus process
-   stim_proc: process
+   stim_proc_w: process
    begin		
       -- hold reset state for 100 ns.
       wait for 100 ns;	
 
       wait for FIFO_W_ACLK_period*10;
 
-      --------------------------------------------- CC : 
-		FIFO_ARESETN <= '1';
-		FIFO_WEN 	 <= '0';
-		FIFO_DATA_IN <= "00000000000000000000000000000000";
-		wait for FIFO_W_ACLK_period*1;
-		
-		--------------------------------------------- CC : 
-		FIFO_ARESETN <= '1';
-		FIFO_REN 	 <= '0';
-		wait for FIFO_R_ACLK_period*10;
-		
+      --------------------------------------------- CC 1 : RESET 
+	  FIFO_ARESETN <= '0';
+	  FIFO_WEN 	 <= '0';
+	  FIFO_DATA_IN <= "00000000000000000000000000000000";
+	  wait for FIFO_W_ACLK_period*1;
+	  
+	  --------------------------------------------- CC 2-10 : Nop for 9 CCs 
+      FIFO_ARESETN <= '1';
+      FIFO_WEN     <= '0';
+      FIFO_DATA_IN <= "00000000000000000000000000000000";
+      wait for FIFO_W_ACLK_period*9;
+        
+      --------------------------------------------- CC 11 : Push 1  
+      FIFO_WEN     <= '1';
+      FIFO_DATA_IN <= "00000000000000000000000000000001";
+      wait for FIFO_W_ACLK_period*1;
+      
+      --------------------------------------------- CC 12 : Push 2  
+      FIFO_WEN     <= '1';
+      FIFO_DATA_IN <= "00000000000000000000000000000010";
+      wait for FIFO_W_ACLK_period*1;
+      
+      --------------------------------------------- CC 13 : Push 3  
+      FIFO_WEN     <= '1';
+      FIFO_DATA_IN <= "00000000000000000000000000000011";
+      wait for FIFO_W_ACLK_period*1;
+      
+      --------------------------------------------- CC 14 : Push 4 
+      FIFO_WEN     <= '1';
+      FIFO_DATA_IN <= "00000000000000000000000000000100";
+      wait for FIFO_W_ACLK_period*1;
+      
+      --------------------------------------------- CC 15 : Push 5
+      FIFO_WEN     <= '1';
+      FIFO_DATA_IN <= "00000000000000000000000000000101";
+      wait for FIFO_W_ACLK_period*1;
+      
+      --------------------------------------------- CC 16 : Push 6
+      FIFO_WEN     <= '1';
+      FIFO_DATA_IN <= "00000000000000000000000000000110";
+      wait for FIFO_W_ACLK_period*1;
+      
+      --------------------------------------------- CC 17 : Push 7
+      FIFO_WEN     <= '1';
+      FIFO_DATA_IN <= "00000000000000000000000000000111";
+      wait for FIFO_W_ACLK_period*1;
+      
+      --------------------------------------------- CC 15 : Push 8
+      FIFO_WEN     <= '1';
+      FIFO_DATA_IN <= "00000000000000000000000000001000";
+      wait for FIFO_W_ACLK_period*1;
+      
+      --------------------------------------------- CC 15-. : Nop 
+      FIFO_WEN     <= '0';
+      FIFO_DATA_IN <= "00000000000000000000000000000000";
+      wait for FIFO_W_ACLK_period*1;
+      
       wait;
    end process;
 
+    -- Stimulus process
+   stim_proc_r: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+      wait for FIFO_R_ACLK_period*10;
+		
+	  ------------------------------------------- CC 1-20 : Nop for 20 CCs  
+      FIFO_REN     <= '0';
+      wait for FIFO_R_ACLK_period*3;
+      
+      ------------------------------------------- CC 21 : Pop 1  
+      FIFO_REN     <= '1';
+      wait for FIFO_R_ACLK_period*1;
+      
+      ------------------------------------------- CC 22 : Pop 2 
+      FIFO_REN     <= '1';
+      wait for FIFO_R_ACLK_period*1;
+      
+      ------------------------------------------- CC 23 : Pop 3
+      FIFO_REN     <= '1';
+      wait for FIFO_R_ACLK_period*1;
+      
+      ------------------------------------------- CC 24 : Pop 4
+      FIFO_REN     <= '1';
+      wait for FIFO_R_ACLK_period*1;
+   
+      ------------------------------------------ CC 25 : Error Empty
+      FIFO_REN     <= '1';
+      wait for FIFO_R_ACLK_period*1;
+      
+      ------------------------------------------ CC 26-. : Nop
+      FIFO_REN     <= '0';
+      wait for FIFO_R_ACLK_period*1;
+      
+      wait;
+   end process;
 END;
