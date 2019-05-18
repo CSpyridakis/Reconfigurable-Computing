dedc7a8d28d661d1f6c8a2dfcfaae0ded51ee031
Spyridakis Christos <spyridakischristos@gmail.com>
2019-05-18 18:00:22 +0300

    simulation screenshots

--- Milestone-2/src/my_ip_hls_tb.cpp
+++ Milestone-2/src/my_ip_hls_tb.cpp
@@ -7,14 +7,25 @@
 
 using namespace hls;
 
+// Custom simulation
 // Packets that we are going to test and static rules that we want to test
-#define static0Rule 70
-#define static1Rule 10
-#define static2Rule 30
-#define STREAM_TEST_ITERATIONS 30
-uint32 MDATA [STREAM_TEST_ITERATIONS] = {10,15,20,25,30,35,40,45,50,55,
-										 60,65,70,65,60,55,50,45,40,35,
-										 30,25,20,15,10,15,20,25,30,35};
+//#define static0Rule 70
+//#define static1Rule 10
+//#define static2Rule 30
+//#define STREAM_TEST_ITERATIONS 30
+//uint32 MDATA [STREAM_TEST_ITERATIONS] = {10,15,20,25,30,35,40,45,50,55,
+//										 60,65,70,65,60,55,50,45,40,35,
+//										 30,25,20,15,10,15,20,25,30,35};
+
+//// Reference Simulation
+//// Packets that we are going to test and static rules that we want to test
+#define static0Rule 10
+#define static1Rule 30
+#define static2Rule 50
+#define STREAM_TEST_ITERATIONS 8
+uint32 MDATA [STREAM_TEST_ITERATIONS] = {10,20,30,40,50,60,70,50};
+
+#define DEBUG 1
 
 int main() {
 
@@ -23,6 +34,18 @@ int main() {
 	stream<axiWord> masterOut("masterOut");
 
 	uint32 rule0cnt = 0, rule1cnt = 0, rule2cnt = 0;		// Rule Counters
+
+	if (DEBUG){
+		printf("\n\nRule0: %d\t| Rule1: %d\t| Rule2: %d\nStream Data:\n", (int)static0Rule,(int)static1Rule,(int)static2Rule);
+		for (i=0;i<STREAM_TEST_ITERATIONS;i++){
+			if (i+1<STREAM_TEST_ITERATIONS)
+				printf(" %d,", (int)MDATA[i]);
+			else
+				printf(" %d", (int)MDATA[i]);
+			if (i%10==0 && i!=0)
+				printf("\n");
+		}
+	}
 
 	printf("\n---------------------------------\nBegin Simulation...\n");
 
